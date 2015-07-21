17dded889d358fb6a46fe5de87da77275edc068a
jijojosein <jijojosejjvilla@gmail.com>
2015-07-21 06:37:40 +0530

    Short Film Finder

---
+++ js/film_db.js
@@ -0,0 +1,13 @@
+var inception = {
+    filmName: "Inception",
+    filmDuration: "90 Mins",
+    filmGenre: "Sci-Fi",
+    filmDirector: ""
+};
+
+var longestride = {
+    filmName: "The Longest Ride",
+    filmDuration: "128 minutes",
+    filmGenre: "Romance",
+    filmDirector: "George Tillman, Jr."
+};
---
+++ js/script.js
@@ -0,0 +1,5 @@
+<script>
+document.getElementsByClassName("searchBtn").addEventListener("click", function(){
+    alert("Hello World!");
+});
+</script>
